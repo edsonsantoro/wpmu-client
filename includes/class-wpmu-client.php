cb0f0f78729d30b3d4b65db7b644aecb6db29ec2
Santoro <edsonsantoro@gmail.com>
2023-03-07 14:43:21 -0300

    changes to how the field is added

--- includes/class-wpmu-client.php
+++ includes/class-wpmu-client.php
@@ -155,7 +155,7 @@ class Wpmu_Client {
 		$plugin_admin = new Wpmu_Client_Admin( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		//$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'wpmu_new_blog', $plugin_admin, 'add_new_blog_field' );
 		$this->loader->add_action( 'network_site_new_form', $plugin_admin, 'my_admin_scripts' );
 

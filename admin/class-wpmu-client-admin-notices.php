9c374971fa2df47237a8d1dd9e92f5b115d6f39e
Santoro <edsonsantoro@gmail.com>
2023-03-15 11:35:42 -0300

    New admin notice class

---
+++ admin/class-wpmu-client-admin-notices.php
@@ -0,0 +1,47 @@
+<?php
+
+class Wpmu_Client_Admin_Notice {
+
+    /**
+     * The message of the notice
+     *
+     * @var string
+     */
+    private $_message = '';
+
+    /**
+     * The type of the message, can be error, info, warning or success
+     *
+     * @var string 
+     */
+    private $_type = 'info';
+
+    /**
+     * If the notice can be dismissible or not.
+     *
+     * @var bool
+     */
+    private $_dismissible = true;
+
+    /**
+     * [Description for __construct]
+     *
+     * @param string $message The message of the notice. Empty string my default.
+     * @param string $type The type of notice (error, info, warning or success). Info by default
+     * @param bool $dismissible If the message is dismissible or not. True by default
+     * 
+     */
+    function __construct( $message = '', $type = 'info', $dismissible = true ) {
+        $this->_message = $message;
+        $this->_dismissible = $dismissible;
+        $this->_type = $type;
+
+        add_action( 'admin_notices', array( $this, 'render' ) );
+    }
+
+    function render() {
+        $dismiss = '';
+        if($this->_dismissible) $dismiss = 'is-dismissible';
+        printf( '<div class="notice notice-'.$this->_type.' '.$dismiss.'"><p>%s</p></div>', $this->_message );
+    }
+}

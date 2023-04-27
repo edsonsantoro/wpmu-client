8529bc4788522b5102e304b8015e4c7a39e1a63c
Santoro <edsonsantoro@gmail.com>
2023-04-27 11:42:03 -0300

    Exclusão de função não necessária

--- admin/js/wpmu-client-form.js
+++ admin/js/wpmu-client-form.js
@@ -1,17 +1,3 @@
 (function( $ ) {
 	'use strict';
-    $(document).ready(function () {
-        $('<tr class="form-field form-required"></tr>')
-            .append($('<th scope="row">Cliente</th>'))
-            .append(
-                $("<td></td>")
-                    .append(
-                        $(
-                            '<input class="regular-text" type="text" title="Cliente" name="blog[client]">'
-                        )
-                    )
-                    .append($("<p>O Cliente deste site</p>"))
-            )
-            .insertAfter("#wpbody-content table tr:eq(2)");
-    });
 })( jQuery );

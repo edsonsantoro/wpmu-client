55b590409eb64b6e9fc88c6a0ed4ce3b64436adf
Edson Santoro <edsonsantoro@gmail.com>
2023-08-29 10:49:33 -0300

    reformat and added log option

--- admin/class-wpmu-client-admin-process.php
+++ admin/class-wpmu-client-admin-process.php
@@ -2,51 +2,139 @@
 
 namespace Wpmu_Client;
 
-/* An easy way to keep in track of external processes.
+/**
+ *  An easy way to keep in track of external processes.
  * Ever wanted to execute a process in php, but you still wanted to have somewhat controll of the process ? Well.. This is a way of doing it.
  * @compability: Linux only. (Windows does not work).
  * @author: Peec
  */
-class Process{
+class Process
+{
+    /**
+     * Summary of pid
+     * @var 
+     */
     private $pid;
+    /**
+     * Summary of command
+     * @var 
+     */
     private $command;
+    /**
+     * Log to write output
+     * @var 
+     */
+    private $log = '/dev/null';
 
-    public function __construct($cl=false){
-        if ($cl != false){
+
+    /**
+     * Summary of __construct
+     * @param mixed $cl
+     */
+    public function __construct($cl = false, $log = false)
+    {
+        if ($cl != false) {
             $this->command = $cl;
-            $this->runCom();
         }
+
+        if ($log != false) {
+            $this->log = $log;
+        }
+
+        $this->runCom();
     }
-    private function runCom(){
-        $command = 'nohup '.$this->command.' > /dev/null 2>&1 & echo $!';
-        exec($command ,$op);
-        $this->pid = (int)$op[0];
+    /**
+     * Summary of runCom
+     * @return void
+     */
+    private function runCom()
+    {
+        $command = 'nohup ' . $this->command . ' > ' . $this->log . ' 2>&1 & echo $!';
+        exec($command, $op);
+        $this->pid = (int) $op[0];
     }
 
-    public function setPid($pid){
+    /**
+     * Set the log file path for the object.
+     *
+     * @param string $log The absolute or relative path to the log file.
+     * @return bool If the log was applied or not
+     */
+    public function setLog(string $log)
+    {
+        if (!empty($log)) {
+            $this->log = $log;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Return the log path
+     *
+     * @return string The log path or empty string if not set
+     */
+    public function getLog()
+    {
+        return $this->log;
+    }
+
+    /**
+     * Summary of setPid
+     * @param mixed $pid
+     * @return void
+     */
+    public function setPid($pid)
+    {
         $this->pid = $pid;
     }
 
-    public function getPid(){
+    /**
+     * Summary of getPid
+     * @return mixed
+     */
+    public function getPid()
+    {
         return $this->pid;
     }
 
-    public function status(){
-        $command = 'ps -p '.$this->pid;
-        exec($command,$op);
-        if (!isset($op[1]))return false;
-        else return true;
+    /**
+     * Summary of status
+     * @return bool
+     */
+    public function status()
+    {
+        $command = 'ps -p ' . $this->pid;
+        exec($command, $op);
+        if (!isset($op[1]))
+            return false;
+        else
+            return true;
     }
 
-    public function start(){
-        if ($this->command != '')$this->runCom();
-        else return true;
+    /**
+     * Summary of start
+     * @return bool
+     */
+    public function start()
+    {
+        if ($this->command != '')
+            $this->runCom();
+        else
+            return true;
     }
 
-    public function stop(){
-        $command = 'kill '.$this->pid;
+    /**
+     * Summary of stop
+     * @return bool
+     */
+    public function stop()
+    {
+        $command = 'kill ' . $this->pid;
         exec($command);
-        if ($this->status() == false)return true;
-        else return false;
+        if ($this->status() == false)
+            return true;
+        else
+            return false;
     }
 }

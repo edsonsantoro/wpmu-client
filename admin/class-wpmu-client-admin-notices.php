97c68c968431003f064ad16443c41d2a5367fad8
Edson Del Santoro <edsonsantoro@gmail.com>
2024-03-19 19:55:35 -0300

    rework on notice class

--- admin/class-wpmu-client-admin-notices.php
+++ admin/class-wpmu-client-admin-notices.php
@@ -5,50 +5,55 @@ namespace Wpmu_Client;
 class Notice {
 
     /**
-     * The message of the notice
+     * The message of the notice.
      *
      * @var string
      */
-    private $_message = '';
+    private $message;
 
     /**
-     * The type of the message, can be error, info, warning or success
+     * The type of the message, can be error, info, warning or success.
      *
      * @var string 
      */
-    private $_type = 'info';
+    private $type;
 
     /**
      * If the notice can be dismissible or not.
      *
      * @var bool
      */
-    private $_dismissible = true;
+    private $dismissible;
 
     /**
-     * [Description for __construct]
+     * Constructor.
      *
-     * @param string $message The message of the notice. Empty string my default.
-     * @param string $type The type of notice (error, info, warning or success). Info by default
-     * @param bool $dismissible If the message is dismissible or not. True by default
+     * @param string|null $message The message of the notice.
+     * @param string $type The type of notice (error, info, warning or success).
+     * @param bool $dismissible If the message is dismissible or not.
      * @param bool $network True if the message is for network admins, false for site admins.
-     * 
      */
-    function __construct( string $message = null, string $type = 'info', bool $dismissible = true, bool $network = false ) {
-        $this->_message = $message;
-        $this->_dismissible = $dismissible;
-        $this->_type = $type;
-        if($network) {
-            add_action( 'network_admin_notices', [$this, 'render'], 5 );    
-        } else {
-            add_action( 'admin_notices', [$this, 'render'], 5  );
+    public function __construct(?string $message = null, string $type = 'info', bool $dismissible = true, bool $network = false) {
+        if ($message !== null) {
+            $this->message = $message;
         }
-        
+        $this->type = $type;
+        $this->dismissible = $dismissible;
+
+        if ($network) {
+            add_action('network_admin_notices', [$this, 'render'], 10);    
+        } else {
+            add_action('admin_notices', [$this, 'render'], 10);
+        }
     }
 
-    function render() {
-        $dismiss = '';
-        if($this->_dismissible) $dismiss = 'is-dismissible';
-        echo '<div class="notice notice-'.$this->_type.' '.$dismiss.'"><p>'.$this->_message.'</p></div>';
+    /**
+     * Render the notice.
+     */
+    public function render() {
+        if (!empty($this->message)) {
+            $dismiss = ($this->dismissible) ? 'is-dismissible' : '';
+            echo '<div class="notice notice-' . $this->type . ' ' . $dismiss . '"><p>' . $this->message . '</p></div>';
+        }
     }
 }
